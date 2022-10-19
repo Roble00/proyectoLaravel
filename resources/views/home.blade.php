f9180b004c05e822b0d8f026d427f01f003e879e
JCode <erobleo@gmail.com>
2022-10-19 16:13:10 +0200

    corrections

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,11 @@
 
 @section('content')
 
-    <!-- bootstrap -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+<!-- bootstrap -->
+@vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
-    <h1>Bienvenidos a las recetas de casa</h1>
-    <a href="{{route('recetas.index')}}">Acceder</a> <!-- apunta al name('recetas.index') -->
+<h1>Bienvenidos a las recetas de casa</h1>
+<a href="{{route('recetas.index')}}">Acceder</a> <!-- apunta al name('recetas.index') -->
+
 
 @endsection()
